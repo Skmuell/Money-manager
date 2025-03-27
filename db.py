18b34feaa891f86f06fccf4ed1d1c49446980d0d
Skmuell <joao.samuellc@gmail.com>
2025-03-27 14:43:34 -0300

    feat: criando tabela usuário

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
@@ -12,6 +13,7 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(bind=engine)
 
+Base = declarative_base()
 
 @contextmanager
 def get_session():
